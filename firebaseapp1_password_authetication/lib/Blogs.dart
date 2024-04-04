cda1ef164a2428cffa5bd7a791c91ca03f1b9696
warishaaslam910 <warishaaslam123@gmail.com>
2024-04-04 15:09:03 +0500

    updated

--- firebaseapp1_password_authetication/lib/Blogs.dart
+++ firebaseapp1_password_authetication/lib/Blogs.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebaseapp1_password_authetication/Login.dart';
--- flutterapp_provider/android/app/src/main/kotlin/com/example/flutterapp_provider/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutterapp_provider
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
